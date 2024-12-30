a7debd765da87213da49902d8b2576ab04e46b56
Dave Summer <nyaridavid345@gmail.com>
2024-12-30 10:30:07 +0100

    Bug fix #38

--- js/global.js
+++ js/global.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const isLoggedIn = localStorage.getItem('loggedInUser');
 
-    if(!isLoggedIn) window.location.href = 'DaveSummer36.github.io/civilwars-test/auth/login.html';
+    if(!isLoggedIn) window.location.href = './auth/login';
 });
