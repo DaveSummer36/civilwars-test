5f25cc9d8b03df67eba098233cd7bf75c1bf17c7
Dave Summer <nyaridavid345@gmail.com>
2024-12-30 10:28:54 +0100

    Bug fix #37

--- auth/js/auth.js
+++ auth/js/auth.js
@@ -68,7 +68,7 @@ function register() {
     localStorage.setItem('users', JSON.stringify(users));
 
     alert('Registration successful! Redirecting to login page...');
-    window.location.href = 'DaveSummer36.github.io/civilwars-test/auth/login.html';
+    window.location.href = './login.html';
 }
 
 if (loginForm) {
