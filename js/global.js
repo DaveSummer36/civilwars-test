7dfcd06e27e2a17c9abfec6de75dcdb011616e28
Dave Summer <nyaridavid345@gmail.com>
2025-01-08 09:18:03 +0100

    News feed updated + bug fixes

--- js/global.js
+++ js/global.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const isLoggedIn = localStorage.getItem('loggedInUser');
 
-    if(!isLoggedIn) window.location.href = './auth/login';
+    if(!isLoggedIn) window.location.href = './auth/login.html';
 });
